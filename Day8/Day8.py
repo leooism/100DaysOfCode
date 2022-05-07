2a194e84e0ca9716c0e5cc3f0c4af665fc245a33
Bidhan Bhandari <leooism10@gmail.com>
2022-05-07 20:39:33 +0545

    added some integer functions

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,5 +1,17 @@
+from cmath import sqrt
+
+
 print("This is my first commit")
 first_name = "Bidhan"
 last_name = "Bhandari"
 full_name = f'{first_name} {last_name}'
 print(full_name)
+
+number_one = 12
+number_two = 13
+sum_number = number_one + number_two
+
+sqr_number = sqrt(number_one)
+pow_number = pow(number_one, 0)
+print(pow_number)
+
