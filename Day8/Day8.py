6c69203e9dd255cc462f8951a129603a968893f7
Bidhan Bhandari <leooism10@gmail.com>
2022-05-07 23:06:06 +0545

    Added certain functionality in Integer

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,17 +1,164 @@
-from cmath import sqrt
+# Operators
+# Integers
+# Strings methods
+
+# Integers
+# from cmath import sqrt
 
 
-print("This is my first commit")
-first_name = "Bidhan"
-last_name = "Bhandari"
-full_name = f'{first_name} {last_name}'
-print(full_name)
+# my_age = 18
+# shallina_age = 16
+# total_age = my_age + shallina_age
+# print(total_age)
 
-number_one = 12
-number_two = 13
-sum_number = number_one + number_two
 
-sqr_number = sqrt(number_one)
-pow_number = pow(number_one, 0)
-print(pow_number)
+# num = 4 
+# print(num * 5)
+
+# sqrt(num1)
+# print(num / 2)
+
+# print(num % 4)
+
+
+# number1 = 16
+# number1 = 16 + 5
+# number1 += 5
+# number1 %= 4
+
+
+print(pow(4, 2))
+
+
+#Power, sqrt, round, 
+# print("Square root ")
+
+num1  = 16
+# num2 = sqrt(num1)
+# print(num2)
+
+num3 = int("4")
+
+print(round(16.9))
+print(round(16.2))
+
+lenght  = len("Bidhan")
+print(lenght)
+
+
+moto  = "100 Days of Code"
+
+print(len(moto))
+
+
+print(moto.upper())
+
+print(moto.lower())
+
+print(moto.casefold())
+
+print("Hello " + "World")
+#SLicing
+#Jack
+#0123
+
+greetings = "Welcome to Nepal"
+print(greetings[11:16])
+
+
+print(greetings[0])
+
+name = input("Enter your name")
+print(name)
+
+
+# greetings[0] = "M" ------Immutable In nature 
+
+print('"Harry potter" is awesome')
+
+#Escaping chter
+#/n``
+#/r
+#/t
+
+print('Hello "Ritik"')
+print("To \n The Principal")
+print("To \t The principal")
+
+print("Hello \"Hello\" world")
+
+
+# print(f"My age is {my_age}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from cmath import sqrt
+
+
+# print("This is my first commit")
+# first_name = "Bidhan"
+# last_name = "Bhandari"
+# full_name = f'{first_name} {last_name}'
+# print(full_name)
+
+# number_one = 12
+# number_two = 13
+# sum_number = number_one + number_two
+
+# sqr_number = sqrt(number_one)
+# pow_number = pow(number_one, 0)
+# print(pow_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
